871636723a3a1cf10f7783b9fd051d83b3615e01
butomo1989 <budi.ut.1989@gmail.com>
2017-01-12 15:56:18 +0100

    Formatted and fixed report.py

--- src/report.py
+++ src/report.py
@@ -34,53 +34,51 @@ def generate_report(distribution_csv, template_file, report_file):
     """
     with open(distribution_csv, 'r') as dc:
         content = csv.reader(dc)
-        table = [','.join(t) for t in content]
+        distribution = [','.join(t) for t in content]
 
         j2_env = Environment(loader=FileSystemLoader(WORK_DIR), trim_blocks=True)
 
         res = requests.get(url=STAT_URL)
         logger.info('request code for {url} is {status}'.format(url=STAT_URL, status=res.status_code))
         if res.ok:
-            stats = res.json()
-            logger.info('json response : {res}'.format(res=stats))
+            json_res = res.json()
+            logger.info('json response : {res}'.format(res=json_res))
 
             with open(report_file, 'w') as rf:
-                for stat in stats['stats']:
-                    num_requests = stat['num_requests']
-                    num_failures = stat['num_failures']
-                    median_response_time = stat['median_response_time']
-                    avg_response_time = stat['avg_response_time']
-                    min_response_time = stat['min_response_time']
-                    max_response_time = stat['max_response_time']
-                    avg_content_length = stat['avg_content_length']
-                    current_rps = stat['current_rps']
+                s_methods, s_names, s_num_req, s_failures, s_median,  s_avg, s_min, s_max, s_length, s_rps =  \
+                    ([] for _ in range(10))
 
-                errors = stats['errors']
-                error_type, error_occur, error_method, error_name = ([] if errors else '-' for _ in range(4))
+                for stat in json_res['stats']:
+                    s_methods.append(stat['method'])
+                    s_names.append(stat['name'])
+                    s_num_req.append(stat['num_requests'])
+                    s_failures.append(stat['num_failures'])
+                    s_median.append(stat['median_response_time'])
+                    s_avg.append(round(float(stat['avg_response_time']), 1))
+                    s_min.append(stat['min_response_time'])
+                    s_max.append(stat['max_response_time'])
+                    s_length.append(round(float(stat['avg_content_length']), 1))
+                    s_rps.append(round(float(stat['current_rps']), 1))
+
+                errors = json_res['errors']
+                e_method, e_name, e_occurences, e_description = ([] if errors else '-' for _ in range(4))
                 if errors:
                     for error in errors:
-                        error_type.append(error['error'])
-                        error_occur.append(error['occurences'])
-                        error_method.append(error['method'])
-                        error_name.append(error['name'])
+                        e_method.append(error['method'])
+                        e_name.append(error['name'])
+                        e_occurences.append(error['occurences'])
+                        e_description.append(error['error'])
 
                 rf.write(j2_env.get_template(template_file).render(
-                    users=stats.get('user_count'), date=str(datetime.date.today()),
-                    error_type=error_type,
-                    error_occur=error_occur,
-                    error_method=error_method,
-                    error_name=error_name,
-                    slaves=round(float(stats.get('slave_count')), 1), rps=round(float(stats.get('total_rps')), 1),
-                    fails=round(float(stats.get('fail_ratio')), 1), num_requests=round(float(num_requests), 1),
-                    num_failures=round(float(num_failures), 1), method=stats['stats'][0]['method'],
-                    name=stats["stats"][0]['name'], median_response_time=round(float(median_response_time), 1),
-                    avg_response_time=round(float(avg_response_time), 1),
-                    min_response_time=round(float(min_response_time), 1),
-                    max_response_time=round(float(max_response_time), 1),
-                    avg_content_length=round(float(avg_content_length), 1),
-                    current_rps=round(float(current_rps), 1),
-                    table_columns=table[0],
-                    table_row=table[1:]))
+                    datetime=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+                    slaves=round(float(json_res.get('slave_count')), 1),
+                    rps=round(float(json_res.get('total_rps')), 1),
+                    fails=round(float(json_res.get('fail_ratio')), 1),
+                    stat_methods=s_methods, stat_names=s_names, stat_num_requests=s_num_req,
+                    stat_num_failures=s_failures, stat_median_res=s_median, stat_avg_res=s_avg,
+                    stat_min_res=s_min, stat_max_res=s_max, stat_content_length=s_length, stat_rps=s_rps,
+                    error_method=e_method, error_name=e_name, error_occur=e_occurences, error_description=e_description,
+                    distribution_header=distribution[0], distribution_content=distribution[1:]))
 
 
 def download_report():
