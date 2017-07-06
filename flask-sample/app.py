fb285a005ef7aae2eea18ed45b85542829825871
budi utomo <budi.utomo@zalando.de>
2017-07-06 16:54:44 +0200

    Added sample flask-app

---
+++ flask-sample/app.py
@@ -0,0 +1,14 @@
+import json
+
+from flask import Flask
+from werkzeug.wrappers import Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return Response(response=json.dumps({'Hello': 'World'}), content_type='application/json')
+
+if __name__ == '__main__':
+    app.run()
