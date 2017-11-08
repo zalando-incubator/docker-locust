1f9645ab5b518c39aae44a2a284a80383425dcae
budi utomo <budi.utomo@zalando.de>
2017-11-08 12:14:30 +0100

    print_help should not called in specific function

--- local.sh
+++ local.sh
@@ -10,14 +10,16 @@ getAbort() {
 trap 'getAbort; exit' SIGINT SIGTERM
 
 function print_help() {
-echo "Usage:
-./local.sh deploy|test --target=<target_url> --locust_file=<file[,file2...]> --slaves=<slaves_amount> --mode=<manual|auto> [--users=<simultaneous_users> --hatch_rate=<added_users_per_second> --duration=<seconds>]
-E.g.:
-./local.sh deploy --target=https://host-to-test.com --locust_file=https://my.storage/file.py,https://my.storage/payload.json --slaves=4 --mode=manual
-./local.sh deploy --target=https://host-to-test.com --locust_file=./local_path.py --slaves=4 --mode=auto --users=1000 --hatch_rate=10 --duration=300
-Using positional args:
-./local.sh deploy https://host-to-test.com ./local_path.py 4 auto 1000 10 300
-"
+    echo "Usage:
+    ./local.sh deploy|test --target=<target_url> --locust_file=<file[,file2...]> --slaves=<slaves_amount> --mode=<manual|auto> [--users=<simultaneous_users> --hatch_rate=<added_users_per_second> --duration=<seconds>]
+
+    E.g.:
+    ./local.sh deploy --target=https://host-to-test.com --locust_file=https://my.storage/file.py,https://my.storage/payload.json --slaves=4 --mode=manual
+    ./local.sh deploy --target=https://host-to-test.com --locust_file=./local_path.py --slaves=4 --mode=auto --users=1000 --hatch_rate=10 --duration=300
+
+    Using positional args:
+    ./local.sh deploy https://host-to-test.com ./local_path.py 4 auto 1000 10 300
+    "
 }
 
 function parse_args() {
@@ -161,6 +163,5 @@ EOF
     fi
 }
 
-[ ! -z "$1" ] && $@
-
 print_help
+$@
