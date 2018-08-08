778dbe7c32f775988160863f61059daea26cea4c
Oliwia Zaremba <oliwiagolec@gmail.com>
2018-08-08 18:11:09 +0200

    Try with -V

--- local.sh
+++ local.sh
@@ -164,11 +164,11 @@ EOF
         else
             echo "Locust will be available on http://<docker-host-ip-address>:8089"
         fi
-
+        pwd
         (export IMAGE=$IMAGE && export TARGET_HOST=$TARGET && export LOCUST_FILE=$LOCUST_FILE && export SLAVE_NUM=$SLAVES &&
         export AUTOMATIC=$AUTOMATIC && export USERS=$USERS && export HATCH_RATE=$HATCH_RATE &&
         export DURATION=$DURATION && export OAUTH=$OAUTH && URL=$URL && export SEND_ANONYMOUS_USAGE_INFO=$KPI &&
-        export SCOPES=$SCOPES && export BUILD_URL=$BUILD_URL && docker-compose up $ABORT_ON_EXIT)
+        export SCOPES=$SCOPES && export BUILD_URL=$BUILD_URL && docker-compose up -V $ABORT_ON_EXIT)
 
         if $AUTOMATIC; then
             docker cp docker_locusts_controller:/opt/reports .
