f5be70560f8ffc72e2830b40d0d32fbed1907152
Oliwia Zaremba <oliwiagolec@gmail.com>
2018-08-08 18:22:33 +0200

    Try --force-recreate

--- local.sh
+++ local.sh
@@ -168,7 +168,7 @@ EOF
         (export IMAGE=$IMAGE && export TARGET_HOST=$TARGET && export LOCUST_FILE=$LOCUST_FILE && export SLAVE_NUM=$SLAVES &&
         export AUTOMATIC=$AUTOMATIC && export USERS=$USERS && export HATCH_RATE=$HATCH_RATE &&
         export DURATION=$DURATION && export OAUTH=$OAUTH && URL=$URL && export SEND_ANONYMOUS_USAGE_INFO=$KPI &&
-        export SCOPES=$SCOPES && export BUILD_URL=$BUILD_URL && docker-compose up -V $ABORT_ON_EXIT)
+        export SCOPES=$SCOPES && export BUILD_URL=$BUILD_URL && docker-compose up --force-recreate $ABORT_ON_EXIT)
 
         if $AUTOMATIC; then
             docker cp docker_locusts_controller:/opt/reports .
